2d1000b69b8ad250f0df59b7736d2ec77b0a15a1
Khai Dam <quangkhaidam93@gmail.com>
2020-06-21 13:33:01 +0700

    Add video orientation ios

---
+++ lib/IBSound.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+import 'package:just_audio/just_audio.dart';
+import 'package:rxdart/rxdart.dart';
+
+class IBSound extends StatefulWidget {
+  @override
+  _IBSoundState createState() => _IBSoundState();
+}
+
+class _IBSoundState extends State<IBSound> {
+  AudioPlayer _player;
+
+  @override
+  void initState() {
+    super.initState();
+    AudioPlayer.setIosCategory(IosCategory.playback);
+    _player = AudioPlayer();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return null;
+  }
+}
